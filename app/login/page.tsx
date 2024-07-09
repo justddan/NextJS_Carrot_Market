f7975cb1edeca805c4f4cb37f71659c588b2b852
김동규 <dgorzldd@gmail.com>
2024-07-10 01:14:02 +0900

    Server Action 1. 내부에서 API URL 다루는 법 (예전 버전)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,30 +1,39 @@
-import FormButton from '@/components/form-btn';
-import FormInput from '@/components/form-input';
-import SocialLogin from '@/components/social-login';
+"use client";
+
+import FormButton from "@/components/form-btn";
+import FormInput from "@/components/form-input";
+import SocialLogin from "@/components/social-login";
 
 export default function Login() {
-    return (
-        <div className="flex flex-col gap-10 py-8 px-6">
-            <div className="flex flex-col gap-2 *:font-medium">
-                <h1 className="text-2xl">안녕하세요!</h1>
-                <h2 className="text-xl">Log in with email and password.</h2>
-            </div>
-            <form className="flex flex-col gap-3">
-                <FormInput
-                    type="email"
-                    placeholder="Email"
-                    required
-                    errors={[]}
-                />
-                <FormInput
-                    type="password"
-                    placeholder="Password"
-                    required
-                    errors={[]}
-                />
-                <FormButton loading={false} text="Login" />
-            </form>
-            <SocialLogin />
-        </div>
-    );
+  const onClick = async () => {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username: "nico",
+        password: "1234",
+      }),
+    });
+    console.log(await response.json());
+  };
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Log in with email and password.</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="email" placeholder="Email" required errors={[]} />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          required
+          errors={[]}
+        />
+      </form>
+      <span>
+        <FormButton onClick={onClick} loading={false} text="Log in" />
+      </span>
+      <SocialLogin />
+    </div>
+  );
 }
--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,15 +1,21 @@
 interface FormButtonProps {
-    loading: boolean;
-    text: string;
+  loading: boolean;
+  text: string;
+  onClick: () => void;
 }
 
-export default function FormButton({ loading, text }: FormButtonProps) {
-    return (
-        <button
-            disabled={loading}
-            className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
-        >
-            {loading ? '로딩 중' : text}
-        </button>
-    );
+export default function FormButton({
+  loading,
+  text,
+  onClick,
+}: FormButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={loading}
+      className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
+    >
+      {loading ? "로딩 중" : text}
+    </button>
+  );
 }
