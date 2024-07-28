6123b4e7749aea39103edeb218f8884aac188d73
김동규 <dgorzldd@gmail.com>
2024-07-28 17:23:55 +0900

    Validation 1. zod 라이브러리 설치

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,42 +1,42 @@
-import FormButton from '@/components/form-btn';
-import FormInput from '@/components/form-input';
-import SocialLogin from '@/components/social-login';
+"use client";
+
+import FormButton from "@/components/form-btn";
+import FormInput from "@/components/form-input";
+import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { createAccount } from "./actions";
 
 export default function CreateAccount() {
-    return (
-        <div className="flex flex-col gap-10 py-8 px-6">
-            <div className="flex flex-col gap-2 *:font-medium">
-                <h1 className="text-2xl">안녕하세요!</h1>
-                <h2 className="text-xl">Fill in the form below to join!</h2>
-            </div>
-            <form className="flex flex-col gap-3">
-                <FormInput
-                    type="text"
-                    placeholder="Username"
-                    required
-                    errors={[]}
-                />
-                <FormInput
-                    type="email"
-                    placeholder="Email"
-                    required
-                    errors={[]}
-                />
-                <FormInput
-                    type="password"
-                    placeholder="Password"
-                    required
-                    errors={[]}
-                />
-                <FormInput
-                    type="password"
-                    placeholder="Password Confirm"
-                    required
-                    errors={[]}
-                />
-                <FormButton loading={false} text="Create Account" />
-            </form>
-            <SocialLogin />
-        </div>
-    );
+  const [state, dispatch] = useFormState(createAccount, null);
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Fill in the form below to join!</h2>
+      </div>
+      <form action={dispatch} className="flex flex-col gap-3">
+        <FormInput
+          name="username"
+          type="text"
+          placeholder="Username"
+          required
+        />
+        <FormInput name="email" type="email" placeholder="Email" required />
+        <FormInput
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <FormInput
+          name="confirm_password"
+          type="password"
+          placeholder="Password Confirm"
+          required
+        />
+        <FormButton text="Create Account" />
+      </form>
+      <SocialLogin />
+    </div>
+  );
 }
