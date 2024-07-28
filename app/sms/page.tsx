0ea607bb641a14ce286e6f0d6bed5a0c0b71aa75
김동규 <dgorzldd@gmail.com>
2024-07-28 19:19:05 +0900

    Validation 7. validator 라이브러리 설치 형 변환해주는 coerce

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -4,10 +4,10 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
-import { smsVerification } from "./actions";
+import { smsLogIn } from "./actions";
 
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsVerification, null);
+  const [state, dispatch] = useFormState(smsLogIn, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -15,12 +15,14 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="Phone Number" required />
+        <Input name="phone" type="text" placeholder="Phone Number" required />
         <Input
           name="token"
           type="number"
           placeholder="Verification code"
           required
+          min={100000}
+          max={999999}
         />
         <Button text="Verify" />
       </form>
