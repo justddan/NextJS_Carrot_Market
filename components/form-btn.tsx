662e715bcad9875eae3036e376ab5e27f2a77879
김동규 <dgorzldd@gmail.com>
2024-07-28 13:44:33 +0900

    Server Action 2. useFormStatus

--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,21 +1,19 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
 interface FormButtonProps {
-  loading: boolean;
   text: string;
-  onClick: () => void;
 }
 
-export default function FormButton({
-  loading,
-  text,
-  onClick,
-}: FormButtonProps) {
+export default function FormButton({ text }: FormButtonProps) {
+  const { pending } = useFormStatus();
   return (
     <button
-      onClick={onClick}
-      disabled={loading}
+      disabled={pending}
       className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
     >
-      {loading ? "로딩 중" : text}
+      {pending ? "로딩 중" : text}
     </button>
   );
 }
