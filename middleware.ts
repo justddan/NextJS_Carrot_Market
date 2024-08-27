a2257e61fcc7f0b6ae7fa5eead904f5391ad3703
김동규 <dgorzldd@gmail.com>
2024-08-27 11:17:12 +0900

    Middleware 2. Authentication 미들웨어 + 미들웨어는 Node.js 런타임이 아닌 엣지 런타임으로 돌아간다. + 라이브러리 사용 금지

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/sessions";
+
+interface Routes {
+  [key: string]: Boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
-  console.log("hello");
-  const pathName = request.nextUrl.pathname;
-  if (pathName === "/") {
-    const response = NextResponse.next();
-    response.cookies.set("middleware-cookie", "hello");
-    return response;
-  }
-  if (pathName === "/profile") {
-    return Response.redirect(new URL("/", request.url));
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
   }
 }
 
 export const config = {
-  matcher: ["/", "/profile", "/create-account", "/user/:path*"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
