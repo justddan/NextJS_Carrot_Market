ca002093d1ec4d61b2ba77917051855b28924a9e
김동규 <dgorzldd@gmail.com>
2025-01-22 22:26:07 +0900

    directive

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col gap-3">
         <div className="bg-white shadow-lg p-t rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
-          <button className="w-full bg-black h-10 text-white rounded-sexy-name m-tomato">
-            Submit
-          </button>
+          <input />
+          <a href="#">aaaa</a>
+          <button className="btn">Submit</button>
         </div>
       </div>
     </main>
