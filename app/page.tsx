235187135bae333176533e1d7993adeaf46a2a96
김동규 <dgorzldd@gmail.com>
2025-01-22 21:44:39 +0900

    대체값 / theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,11 @@ export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col gap-3">
-        {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
-          <div key={index} className="flex items-center group gap-5">
-            <div className="size-7 bg-blue-400 rounded-full" />
-            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500">
-              {person}
-            </span>
-            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
-              <span className="z-10">{index}</span>
-              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
-            </div>
-          </div>
-        ))}
+        <div className="bg-white shadow-lg p-t rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+          <button className="w-full bg-black h-10 text-white rounded-sexy-name m-tomato">
+            Submit
+          </button>
+        </div>
       </div>
     </main>
   );
