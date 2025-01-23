f636a7f84e4020bacfb449507f54b34c42cfe550
김동규 <dgorzldd@gmail.com>
2025-01-23 22:13:13 +0900

    API Route(Route Handler)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function Login() {
+  const onClick = async () => {
+    const response = await fetch("api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        name: "kim",
+        password: "1234",
+      }),
+    });
+    console.log(await response.json());
+  };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -17,9 +29,10 @@ export default function Login() {
           required
           errors={[]}
         />
-
-        <FormButton loading={false} text="Create Account" />
       </form>
+      <span onClick={onClick}>
+        <FormButton loading={false} text="Log in" />
+      </span>
       <SocialLogin />
     </div>
   );
