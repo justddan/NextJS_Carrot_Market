ae5dd4694af4419225928d38eea2b287f8105a01
김동규 <dgorzldd@gmail.com>
2025-01-25 01:38:21 +0900

    validator / SMS 페이지

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,3 +1,51 @@
 "use server";
 
-export async function smsVerification(prevState: any, formData: FormData) {}
+import { z } from "zod";
+import validator from "validator";
+import { redirect } from "next/navigation";
+
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format"
+  );
+
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+interface ActionState {
+  token: boolean;
+}
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return {
+        phone: phone?.toString(),
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        phone: phone?.toString(),
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        phone: phone?.toString(),
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
+}
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -3,10 +3,16 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { useActionState } from "react";
-import { smsVerification } from "./actions";
+import { smsLogin } from "./actions";
+
+const initialState = {
+  phone: "",
+  token: false,
+  error: undefined,
+};
 
 export default function SMSLogin() {
-  const [state, dispatch] = useActionState(smsVerification, null);
+  const [state, dispatch] = useActionState(smsLogin, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -14,15 +20,27 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="Phone number" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-        />
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="number"
+            placeholder="Phone number"
+            required
+            defaultValue={state.phone}
+            errors={state.error?.formErrors}
+          />
+        )}
 
-        <Button text="Verify" />
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
