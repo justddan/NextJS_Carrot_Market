b5d0905dffae7e2e924af04006cd9893a52fc043
김동규 <dgorzldd@gmail.com>
2025-01-28 20:29:18 +0900

    갱신하는 두 번째 방법 revalidatePath

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -2,15 +2,12 @@ import ProductList from "@/components/product-list";
 import db from "@/lib/db";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
-import { unstable_cache as nextCache } from "next/cache";
+import { unstable_cache as nextCache, revalidatePath } from "next/cache";
 import Link from "next/link";
 
-const getCachedProducts = nextCache(getInitialProducts, ["home-products"], {
-  revalidate: 60,
-});
+const getCachedProducts = nextCache(getInitialProducts, ["home-products"], {});
 
 async function getInitialProducts() {
-  console.log("hit!!!!");
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -37,10 +34,17 @@ export const metadata = {
 
 export default async function Product() {
   const initialProducts = await getCachedProducts();
+
+  const revalidate = async () => {
+    "use server";
+    revalidatePath("/home");
+  };
   return (
     <div>
-      <Link href="/home/recent">Recent Products</Link>
       <ProductList initialProducts={initialProducts} />
+      <form action={revalidate}>
+        <button>Revalidate</button>
+      </form>
       <Link
         href="/products/add"
         className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
