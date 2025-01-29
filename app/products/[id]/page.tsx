e6572deb98b31eb4996532991bc2ef205d256e8b
김동규 <dgorzldd@gmail.com>
2025-01-29 12:08:31 +0900

    generateStaticParams / NextJS에게 id 미리 알려주기

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -9,19 +9,20 @@ import { notFound } from "next/navigation";
 import { unstable_cache as nextCache, revalidateTag } from "next/cache";
 
 async function getIsOwner(userId: number) {
-  const session = await getSession();
-  if (session.id) return session.id === userId;
+  // const session = await getSession();
+  // if (session.id) return session.id === userId;
+
   return false;
 }
 
-async function getCachedFetch() {
-  fetch("https://api.com", {
-    next: {
-      revalidate: 60,
-      tags: ["hello"],
-    },
-  });
-}
+// async function getCachedFetch() {
+//   fetch("https://api.com", {
+//     next: {
+//       revalidate: 60,
+//       tags: ["hello"],
+//     },
+//   });
+// }
 
 async function getProduct(id: number) {
   const product = await db.product.findUnique({
@@ -143,4 +144,13 @@ export default async function ProductDetail({
       </div>
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+    },
+  });
+  return products.map((product) => ({ id: product.id + "" }));
 }
