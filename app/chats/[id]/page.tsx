d39dad16be719719e3e713a94332840c3b557c8e
김동규 <dgorzldd@gmail.com>
2025-01-31 23:10:59 +0900

    채팅방 퍼블리시

--- app/chats/[id]/page.tsx
+++ app/chats/[id]/page.tsx
@@ -1,5 +1,7 @@
+import ChatMessagesList from "@/components/chat-messages-list";
 import db from "@/lib/db";
 import getSession from "@/lib/session";
+import { Prisma } from "@prisma/client";
 import { notFound } from "next/navigation";
 
 async function getRoom(id: string) {
@@ -22,10 +24,37 @@ async function getRoom(id: string) {
   return room;
 }
 
+async function getMessage(chatRoomId: string) {
+  const messages = await db.message.findMany({
+    where: {
+      chatRoomId,
+    },
+    select: {
+      id: true,
+      payload: true,
+      created_at: true,
+      userId: true,
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return messages;
+}
+
+export type InitialChatMessages = Prisma.PromiseReturnType<typeof getMessage>;
+
 export default async function ChatRoom({ params }: { params: { id: string } }) {
   const room = await getRoom(params.id);
-
   if (!room) return notFound();
 
-  return <h1>chat!</h1>;
+  const initialMessages = await getMessage(params.id);
+  const session = await getSession();
+
+  return (
+    <ChatMessagesList userId={session.id!} initialMessages={initialMessages} />
+  );
 }
