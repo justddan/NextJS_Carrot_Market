649ca40026b6885664f5fcb5969050ebd97e6a3f
김동규 <dgorzldd@gmail.com>
2025-02-03 11:32:49 +0900

    스트리밍 추가하기

---
+++ app/streams/add/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Button from "@/components/button";
+import Input from "@/components/input";
+import { useFormState } from "react-dom";
+import { startStream } from "./actions";
+
+export default function AddStream() {
+  const [state, action] = useFormState(startStream, null);
+  return (
+    <form action={action} className="p-5 flex flex-col gap-2">
+      <Input
+        name="title"
+        required
+        placeholder="Title or your stream."
+        errors={state?.error.formErrors}
+        defaultValue={state?.title}
+      />
+      <Button text="Start streaming" />
+    </form>
+  );
+}
